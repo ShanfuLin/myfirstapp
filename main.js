9b1a3874987d4bc261d50321f2653e4d2e092dc7
ShaunLin <shanfu87@yahoo.com>
2022-10-28 11:02:05 +0800

    My first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// Import packages 
+const express = require('express')
+const morgan = require ('morgan')
+
+// Configure out port 
+//Check if it is set in the environment variable, if not then we default to 3000
+// use process.env.Port ... if not 3000
+const port = process.env.Port || 3000
+
+// Create an instance of express application
+const app = express()
+
+// Log all request
+app.use(morgan('common'))
+
+// GET/
+app.get(`/`, (req, resp) => {
+    // what is the media type
+    resp.type('text/html')
+    resp.status(200)
+    resp.send(`<h1> The current time is ${new Date()} </h1> <h2> This is a modified line! </h2>`)
+})
+
+//Start our web application
+app.listen(port, () => {
+    console.info(`Starting application on port ${port} at ${new Date()}`)
+})
+
